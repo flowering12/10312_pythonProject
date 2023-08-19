a3f85095eecbd4a0b062138f2c19b110aa47aa3a
User <s>
2023-08-19 10:15:41 +0900

    사용자로부터 값을 입력받는 방법

---
+++ pythonBasic/inputVariable.py
@@ -0,0 +1,5 @@
+first_number = input("숫자를 하나 입력") #input은 문자열 입력받아서 return함
+temp_number = int(first_number) + 10
+print("숫자 "+first_number+"를 입력함")
+print(first_number ,"에 10더한 값은",temp_number, "입니다")
+second_number = int(input("다른 숫자도 입력"))
