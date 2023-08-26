d2520dea68e87cb5c0b222534fde45f4e1bc4376
User <s>
2023-08-26 10:47:05 +0900

    continue

--- pythonBasic/forstatement.py
+++ pythonBasic/forstatement.py
@@ -8,7 +8,7 @@ print( strings[0])
 
 print(anys[3][2])
 
-scores = [84, 65, 79, 91, 50, 80, 54, 23, 58, 64, 95, 90, 39, 84, 67, 82, 99, 91]
+scores = [84, -5, 65, 79, 105, 100, 91, 50, -32, 80, 54, 23, 58, 64, 95, 90, 39, 84, 67, 82, 99, 91]
 
 print(scores[3:])
 print(scores[1:4])
@@ -16,9 +16,20 @@ print(scores[:3])
 
 #점수 평균
 sum = 0
+# for i in scores:
+#     if 0 < i < 100:
+#         sum = sum+i
+for i if scores:
+    if i >100 or 1 < 0:
+        print("skip", i)
+        continue
+        sum = sum + i
 
 for i in scores:
     sum = sum + i
     average = sum / len(scores)
     print("점수 합:", sum)
     print("점수 평균:", average)
+
+for i in range(0,11):
+ print(i)
