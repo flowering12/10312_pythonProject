39115dc7a642d10eb40b6bbbccd15bcb56a39726
User <s>
2023-08-26 11:47:34 +0900

    # 사용자가 answer 맞출때까지 반복 # 1. 사용자에게 기회주기 # 2. 틀렸을때 updown 출력해주기

--- pythonBasic/whileStatement.py
+++ pythonBasic/whileStatement.py
@@ -1,17 +1,43 @@
-number = 0
+# number = 0
+#
+# while number < 5:
+#     print(number)
+#     number = number + 1
+#
+# print("=====메뉴=====")
+# print("1. 시작하기")
+# print("2. 종료하기")
+# print("==============")
+#
+# user_input = -1
+#
+# while user_input != 2:
+#     user_input = int(input("값을 입력하세요 >>"))
+#     if user_input == 2:
+#         break
 
-while number < 5:
-    print(number)
-    number = number + 1
+import random
 
-print("=====메뉴=====")
-print("1. 시작하기")
-print("2. 종료하기")
-print("==============")
 
+answer = random.randrange(0, 10)
 user_input = -1
 
-while user_input != 2:
-    user_input = int(input("값을 입력하세요 >>"))
-    if user_input == 2:
-        break
+   user_input == int(input("값을 입력하세요>>")
+   while True:
+       user_input = int(input())
+       if answer == user_input:
+           print('correct')
+           break
+       else:
+           print('try again')
+           if user_input < answer:
+               print('up')
+           else:
+               print('down')
+   if user_input == answer:
+     print("정답입니다!")
+     break
+
+# 사용자가 answer 맞출때까지
+# 1. 사용자에게 기회주기
+# 2. 틀렸을때 updown 출력해주기
